e9aa97bd26434ad4b358bf5ae207dd41b3d84c02
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-09-03 12:39:52 +0000

    updated link to manifest location

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,7 @@
 	<meta name="description" content="Tarzan Enterprise Ltd. is a private limited liability company whose main operational branch is located in Tema, Ghana">
 	@yield('extracss')
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<link rel="manifest" href="/manifest.json">
 </head>
 <body>
 	@include('layout.partials.header')
