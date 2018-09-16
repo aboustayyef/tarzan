e67a996226775679df55661904a58e89758ddd18
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2018-09-16 17:53:48 +0000

    slideshows is now a vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,22 +15,9 @@ window.Axios = require('axios');
 
 // Vue
 window.Vue = require('vue');
+Vue.component('tarzan-slideshow', require('./components/TarzanSlideshow.vue'));
 Vue.component('tarzan-news', require('./components/TarzanNews.vue'));
 
 const app = new Vue({
     el: '#app'
-});
-
-// Flickity
-const Flickity = require('flickity-imagesloaded'); 
-require('flickity-bg-lazyload');
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  imagesLoaded: true,
-  autoPlay: true,
-  bgLazyLoad: true,
-  contain: true
 });
---
+++ resources/assets/js/components/TarzanSlideshow.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="main-carousel">
+            <div v-for="slide in slides" class="carousel-cell carousel-bg-image" :data-flickity-bg-lazyload="slide"></div>
+    </div>
+</template>
+
+<script>
+    // Flickity
+    const Flickity = require('flickity-imagesloaded'); 
+    require('flickity-bg-lazyload');
+
+    export default {
+        data: function () {
+            return {
+              slides: ['/img/slides/tarzan-lorry-with-cocoa-bags.jpg','/img/slides/tarzan-reach-stacker.jpg']
+            }
+        },
+        mounted: function(){
+            var elem = document.querySelector('.main-carousel');
+            var flkty = new Flickity( elem, {
+                // options
+                cellAlign: 'left',
+                imagesLoaded: true,
+                autoPlay: true,
+                bgLazyLoad: true,
+                contain: true
+            });
+        }
+    }
+</script>
