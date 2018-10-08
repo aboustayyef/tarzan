c6748714b866075320db621eb1383daa8ebe5210
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2018-10-08 13:24:20 +0000

    added special page for blue gallery access

---
+++ resources/views/specials/tarzan-bg-access-2018.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blue Gallery Access</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Tarzan Enterprise Ltd. is a private limited liability company whose main operational branch is located in Tema, Ghana">
+	@yield('extracss')
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="theme-color" content="#e83844">
+</head>
+<body>
+	<div class="section">
+		<div class="container content" style="max-width: 700px">
+			<img src="/img/tarzan-logo.png" width="191px" height="auto" alt="tarzan logo">
+			<h1 class="title is-size-4">Public Statement:</h1>
+			<h1 class="title is-size-2">ACCESS TO BLUE GALLERY COCOA WAREHOUSES</h1>
+			<p>In view of the seeming confusion regarding the readiness or accessibility of one of our warehousing facilities, commonly referred to as “Blue Gallery Warehouse”, which is located on the Aflao road towards the Tema Motorway Roundabout, the management of Tarzan Enterprise Limited would like to issue the following clarification to its cherished stakeholders in the cocoa industry in Ghana.</p>
+			<img src="/img/special/tarzan-bg-warehouses-access.png" alt="" style="margin:1em 0;border: 1px solid silver">
+			<div class="columns">
+				<div class="column is-one-half">
+					<p>Despite the ongoing disruption of the normal traffic flow around Tema Motorway Roundabout and its connecting roads, due to the combined effect of the “Tema Intersection Road Expansion Project” and the “Tema-Akosombo Railway Project”, all the warehouse facilities of Tarzan Enterprise Ltd. remain unaffected in their operations.</p>
+					<p>In fact, the company has been assured by the various contractors (Shimizu-Dai Nippon Joint Venture - SDJV, CTI Engineering International Co.) of their willingness to maintain alternative entry/exit points to our affected facilities, throughout the various phases of the ongoing construction work, in order to ensure a continued and uninterrupted access to cocoa-related logistics operations.</p>
+					<p>Of particular concern is the Blue Gallery Warehouse, which is a high-quality facility that is earmarked for the storage of one of the country’s highest grades of export cocoa, referred to as “Japan Cocoa”. While the regular access point has been temporarily blocked for heavy articulators from Aflao road, an alternative entrance has been created by the contractors from the Akosombo road that is pliable by heavy articulators, as indicated in the map below.</p>
+				</div>
+				<div class="column is-one-half">
+					<p> In addition to that, an access point from Aflao road is available for passenger vehicles and SUVs. Moreover, the contractors have also assured us of the restoration of normal access upon completion of their construction project.</p>
+					<p>Therefore, we urge our cherished stakeholders to desist from spreading or communicating false claims regarding the readiness of our facilities and its access roads. Tarzan Enterprise Ltd. takes pride in its long history in supporting the Ghanaian economy and supporting the cocoa value chain with solid, high-quality logistics services since 1979. We assure you of our readiness and wish you all a very fruitful and successful cocoa season, in the interest of our industry and the nation.</p>
+					<p>
+						<hr>
+						<p>October 10, 2018</p>
+						<strong>
+							Ziad M. Hamoui. Executive Director<br>
+							Executive Director, Operations
+						</strong>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
