d1fec8e5c62f49b00aeee43afe010bcdc3dad931
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-05-26 15:30:42 +0000

    re-added vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@ window.Axios = require('axios');
 
 // Vue
 window.Vue = require('vue');
-Vue.component('tarzan-slideshow', require('./components/TarzanSlideshow.vue'));
-Vue.component('tarzan-news', require('./components/TarzanNews.vue'));
+Vue.component('tarzan-slideshow', require('./components/TarzanSlideshow.vue').default);
+Vue.component('tarzan-news', require('./components/TarzanNews.vue').default);
 
 const app = new Vue({
     el: '#app'
